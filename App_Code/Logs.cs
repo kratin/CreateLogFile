3a2761d0cf613d72e7d7ca81bee1d9a2ab631746
kratin <mintra081@gmail.com>
2012-07-10 17:02:10 +0700

    Create Log files

---
+++ App_Code/Logs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.IO;
+using System.Text;
+
+/// <summary>
+/// Summary description for Logs
+/// </summary>
+public class Logs
+{
+    private string sLogFormat;
+    private string sErrorTime;
+
+	public Logs()
+	{
+		
+	}
+    public  void CreateLogFiles(String sPathName,String sErrMsg)
+    {
+        sLogFormat = DateTime.Now.ToShortDateString().ToString() + " " + DateTime.Now.ToLongTimeString().ToString() + "==>";
+        string sYear = DateTime.Now.Year.ToString();
+        string sMonth = DateTime.Now.Month.ToString();
+        string sDay = DateTime.Now.Day.ToString();
+
+        sErrorTime = sYear + sMonth + sDay;
+
+        StreamWriter sw = new StreamWriter(sPathName+sErrorTime,true);
+        sw.WriteLine(sLogFormat + sErrMsg);
+        sw.Flush();
+        sw.Close();
+
+    }
+    
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.IO;
+using log4net;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int a = 23, b = 0;
+
+        try
+        {
+
+            int result = a / b;
+
+        }
+
+        catch (Exception ex)
+        {
+
+            // Divide by Zero exception occurred and in the next line we are going to log that
+            ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            //logger.Debug(ex.Message);
+            logger.Error(ex.Message);
+           
+
+        }
+    }
+}
